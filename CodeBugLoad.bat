e2fcb5659617c50d203af05d977b70f60e97b6e0
jam3sward <jam3sward@users.noreply.github.com>
2015-11-25 22:01:42 +0000

    First import of CodeBugLoad.bat

---
+++ CodeBugLoad.bat
@@ -0,0 +1,68 @@
+@echo off
+
+REM Given the name of a codebug.cbg file, this batch file attempts
+REM to automatically locate the CodeGug drive letter, then copies
+REM the file directly to the CodeBug.
+REM James Ward
+REM 25/11/15
+REM Version 1.0
+
+setlocal ENABLEDELAYEDEXPANSION
+
+REM This is the filename to be transferred to the CodeBug
+set payload=%1
+
+REM Check that we were given a filename
+if "%payload%"=="" (
+  echo Please supply a filename
+  goto exit
+)
+
+REM Check that the supplied file exists
+if not exist %payload% (
+  echo Can't find file %payload%
+  goto exit
+)
+
+REM Get filename extension
+for %%f in (%payload%) do set extension=%%~xf
+
+REM Check filename extension
+if not %extension%==.cbg (
+  echo Filename should have .cbg extension
+  goto exit
+)
+
+REM Now we have to find the CodeBug, so we can transfer
+REM the file onto it
+
+REM Get a list of all the logical disks including name and volumename
+REM for example: E: CodeBug
+wmic logicaldisk get name,volumename |more >disks.txt
+
+REM Find out which one has volume name CodeBug
+set codebug=""
+for /f "tokens=1,2 skip=1" %%i in (disks.txt) do (
+  if %%j==CodeBug set codebug=%%i
+)
+
+REM If we have CodeBug drive letter
+if not %codebug%=="" (
+  REM Check that the README.HTM file exists on that drive
+  if exist %codebug%\README.HTM (
+    REM This looks like a real CodeBug, let's transfer the file
+    echo CodeBug drive letter is %codebug%
+    echo Copying %payload% to %codebug%
+    copy %payload% %codebug% >nul
+  ) else (
+    echo Found what looked like a CodeBug drive letter, but it
+    echo didn't seem to contain the usual README.HTM file, so
+    echo the file wasn't copied
+  )
+) else (
+  echo Didn't find the CodeBug: is it connected and in
+  echo programming mode?
+)
+
+:exit
+pause
